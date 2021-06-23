595966cc8664a0933928b6c489cfb27600443763
Mattyboy84 <41299838+mattyboy84@users.noreply.github.com>
2021-06-23 19:11:48 +0100

    a
    a

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module JavaFxApplication {
+requires javafx.fxml;
+requires javafx.controls;
+
+opens sample;
+
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+    //
+    //1080p
+    int madeWithWidth = 1920;
+    int madeWithHeight = 1080;
+    //
+    int screenWidth = (int) screenBounds.getWidth();
+    int screenHeight = (int) screenBounds.getHeight();
+    //
+    float scaleX = (float) screenWidth / madeWithWidth;
+    float scaleY = (float) screenHeight / madeWithHeight;
+
+    Group group = new Group();
+    Scene scene = new Scene(group, screenWidth, screenHeight);
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
