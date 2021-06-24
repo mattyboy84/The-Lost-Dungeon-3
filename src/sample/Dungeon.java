0a9b8fd106a7548ed1825999d696e69c71950fc9
Mattyboy84 <41299838+mattyboy84@users.noreply.github.com>
2021-06-24 19:41:14 +0100

    dungeon starter

--- src/sample/Dungeon.java
+++ src/sample/Dungeon.java
@@ -1,15 +1,24 @@
 package sample;
 
 public class Dungeon {
-
-
-
-
     public Dungeon() {
     }
 
-    public void Generate() {
+    int mapX = 19;
+    int mapY = 19;
+    int[][] map;
 
+    int startX = (mapX + 1) / 2;
+    int startY = (mapY + 1) / 2;
+
+
+    public void Generate() {
+        map = new int[19][19];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                map[i][j] = 0;
+            }
+        }
 
 
     }
